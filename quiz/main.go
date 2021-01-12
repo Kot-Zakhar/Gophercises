23355feac9c06dadfed478986e49863e84cd4ab9
Zakhar <kot.zakhar@gmail.com>
2021-01-12 21:29:08 +0300

    Adding flags

--- quiz/main.go
+++ quiz/main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/csv"
 	"io"
 	"strings"
+	"flag"
 )
 
 type Task struct {
@@ -39,7 +40,17 @@ func getTasks(filename string) (tasks []Task, err error) {
 }
 
 func main() {
-	var tasks, _ = getTasks("./problems.csv")
+	var filename string
+	var limit int
+
+	flag.StringVar(&filename, "csv", "./problems.csv", "a csv file in the format of 'question,answer' (default \"problems.csv\"")
+	flag.IntVar(&limit, "limit", 30, "the time limit for the quiz in seconds (default 30)")
+
+	flag.Parse()
+
+	var tasks, _ = getTasks(filename)
+	// TODO: add error processing
+
 	var correct = 0
 	var answer string
 
